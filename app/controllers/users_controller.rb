1da055afcf32d015ad7ea4b858a66ee69ee12a82
makale <jacksonmakale@rocketmail.com>
2016-07-01 14:05:33 +0000

    add users listing and update show article view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+  
+  
  def new
  @user = User.new
  end
@@ -27,7 +32,7 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
